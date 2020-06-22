f30315e61153801c9af09b1193d01d052fb9ac6f
Francis Badasu <nytro04@gmail.com>
2020-06-22 16:43:28 +0000

    feat: password reset functionality

--- controller/authController.js
+++ controller/authController.js
@@ -225,4 +225,20 @@ exports.resetPassword = catchAsync(async (req, res, next) => {
     createPasswordResetToken: hashedToken,
     createPasswordResetExpires: { $gt: Date.now() },
   });
+
+  // return error if user is not found
+  if (!user) {
+    return next(new AppError("Token is invalid or has expired", 400));
+  }
+
+  // 2. if token has not expired, and there is a user, set the new password
+
+  user.password = req.body.password;
+  user.passwordConfirm = req.body.passwordConfirm;
+  user.passwordResetToken = undefined;
+  user.passwordResetExpires = undefined;
+  await user.save();
+
+  // 4. Log the user in and send JWT
+  createAndSendToken(user, 200, res);
 });
